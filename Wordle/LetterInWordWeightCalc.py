184bedff3a99bf03555512b7d0bfcb5aa0f661bd
Evan12Scott <evan12scott@gmail.com>
2023-08-07 22:33:02 -0700

    finished data set scaling for range 0 to 1 for both .txt files

--- Wordle/LetterInWordWeightCalc.py
+++ Wordle/LetterInWordWeightCalc.py
@@ -1,31 +1,45 @@
 """ BY EVAN SCOTT"""
-#This program scales the data set: Long_wordlist.txt down to a 0 to
+#This program scales the 2 data set: LetterFrequency.txt and Long_wordlist.txt down to a 0 to
 # 1 range >>> explanation below 
 
-#Long_wordlist.txt - initalize dictionary for the tracking of occurrences for 
+#Long_wordlist.txt - initalize dictionary ({key: [letter 1, letter 2, ... letter 5]}) for
+#the tracking of occurrences for 
 # specific letters in specific spots of the five letter words 
 #divide by the total occurrences of that letter in the list so each probability
-#for each letter in different spots sums to 1 and then apply 
-# sigmoid for proportionality across data set scaling
+#for each letter in different spots sums to 1 for proportionality across data set scaling
 #Example: {A: [], B: []} which will become {A: [40, 32, 19, 24, 3], B: [13, 46, 92, 8, 29]}
 #and finally(assuming probability of a letter being in each spot is equal) 
 # {A: [0.2, 0.2, 0.2, 0.2, 0.2], B: [0.2, 0.2, 0.2, 0.2, 0.2]}
 
+#LetterFrequency (simpler) - initialize dictionary ({key: frequency weight value of letter occurring})
+# and divide those experiment-calculated values by 100 to get the summation of all 
+# probabilities  for all letters to be 1 for proportionality across data set scaling
+#Example: {A: 0.24, B: 0.06, C: 0.19}
+
+import csv
+import numpy as np
+
 f = open("/Users/evanp/OneDrive/Desktop/Individual Projects/WordleRepo/Wordle/long_wordlist.txt", "r")
-dict = {}
-totalLettersDict = {}
+
+d = open("/Users/evanp/OneDrive/Desktop/Individual Projects/WordleRepo/Wordle/LetterFrequency.txt", "r")
+csv_reader = csv.reader(d, delimiter = ',')
+
+dictLWFreq = {}
+totalLettersdictLWFreq = {}
+dictLetterFreq = {}
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 alphabetLength = 26
 
-#track how many of each letter there is
-
-
-#initialize dict
+#initialize dictLWFreq
 for ch in alphabet:
-    dict[ch.upper()] = [0,0,0,0,0]
-    totalLettersDict[ch.upper()] = 0
+    dictLWFreq[ch.upper()] = [0,0,0,0,0]
+    totalLettersdictLWFreq[ch.upper()] = 0
 
+for line in csv_reader:
+    dictLetterFreq[line[0]] = (round((float(line[1]) / 100.0), 6))
+
+print(dictLetterFreq)
 
 for line in f:
     track = 0
@@ -35,19 +49,19 @@ for line in f:
         if char == "\n":
             continue
 
-        totalLettersDict[chKey] +=1
+        totalLettersdictLWFreq[chKey] +=1
 
-        if dict[chKey][track] == 0:
-            dict[chKey][track] = 1
+        if dictLWFreq[chKey][track] == 0:
+            dictLWFreq[chKey][track] = 1
         else:
-            dict[chKey][track] +=1 
+            dictLWFreq[chKey][track] +=1 
             track += 1
 
 i = 0
-for key, value  in dict.items():
+for key, value  in dictLWFreq.items():
     while i < 5:
-        value[i] = (int(value[i]) / totalLettersDict[key])
+        value[i] = (round((int(value[i]) / totalLettersdictLWFreq[key]), 6))
         i+= 1
     i = 0
 
-print(dict)
+print(dictLWFreq)
